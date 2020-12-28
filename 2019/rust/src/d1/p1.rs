ea9ff45c77a78f7b81cc7560e6703cce63ec099d
Richard Hayes <rhshadowgreen@gmail.com>
2020-12-27 18:19:10 -0800

    d1 p1

--- 2019/rust/src/d1/p1.rs
+++ 2019/rust/src/d1/p1.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 pub fn main() {
-    println!("Hello, p1!");
+    let data = fs::read_to_string("src/d1/input.txt")
+        .expect("read failed");
+
+    let lst = data.trim().split("\n");
+
+    let mut ns = Vec::new();
+
+    for s in lst {
+        ns.push(s.parse::<i32>().unwrap());
+    }
+
+    let mut total : i32 = 0;
+
+    for n in ns {
+        total += n / 3 - 2;
+    }
+
+    println!("{}", total);
 }
