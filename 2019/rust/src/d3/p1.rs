75aba7e265c42cfed6bcc2ae47d4f3866d920e4a
Richard Hayes <rhshadowgreen@gmail.com>
2021-01-04 20:00:40 -0800

    d3 partial

--- 2019/rust/src/d3/p1.rs
+++ 2019/rust/src/d3/p1.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
 pub fn main() {
-    println!("Hello, p1!");
+    let fname = "src/d3/input.txt";
+    let data: Vec<Vec<(char, i32)>> = fs::read_to_string(fname)
+        .expect("read failed")
+        .trim().lines() // &str to &[str]
+        .map(|line| { // line: &str
+            line.trim().split(",") // &[str]
+            .map(|t| {
+                t.chars().collect::<Vec<char>>() // &[str] to Vec<char>
+            })
+            .map(|t| { // t: &[char]
+                (
+                    t[0], // First char 'L' or 'R'
+                    t[1..].iter()
+                        .collect::<String>() // &[char] to String
+                        .parse().unwrap() // String to i32
+                )
+            }).collect() // Vec<(char, i32)>
+        }).collect();
+
+    println!("{:?}", data);
 }
